36d745ca17e8d980aaed53e702d472a0f988ddfe
keyur1625 <61237051+keyur1625@users.noreply.github.com>
2020-02-24 11:08:20 +0530

    Update views.py

--- TryOn/views.py
+++ TryOn/views.py
@@ -5,6 +5,8 @@ from TryOn import form as forms
 from TryOn.models import *
 from TryOn import models
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib import messages
+from django.contrib.auth.models import User,auth
 
 @csrf_exempt
 def index(request):  
@@ -177,3 +179,54 @@ class ProductDisplayView(View):
 
     def delete(self, request):
         pass
+    
+ def register(request):
+
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username'] 
+        password1 = request.POST['password1']
+        password2 = request.POST['password2'] 
+        email = request.POST['email']
+
+        if password1==password2:
+            if  User.objects.filter(username=username).exists():
+                messages.info(request,'Username taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,'email taken')
+                return redirect('register')    
+            else:
+                user = User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
+                user.save();
+                print('user created')
+                return redirect('login')
+
+        else:
+            print('password not matching.....')
+            return redirect('register')
+    else:
+        return render(request,'register.html')
+
+def login(request):
+
+    if request.method == 'POST':
+        username = request.POST['username'] 
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+ #           request.sessions['is_logged']= True
+            return redirect("/")
+
+        else:
+            messages.info(request,'invalid credentials')
+            return redirect('login')
+    else:    
+        return render(request,'login.html')        
+def logout(request):
+      auth.logout(request)
+      return redirect('/')   
